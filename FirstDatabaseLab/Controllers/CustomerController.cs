2c0fc8ebfbdab2e86e3befe7c879a10ef8205982
Rely Ky <relyky@hotmail.com>
2025-10-16 00:10:21 +0800

    雞婆

--- FirstDatabaseLab/Controllers/CustomerController.cs
+++ FirstDatabaseLab/Controllers/CustomerController.cs
@@ -36,39 +36,45 @@ public class CustomerController(MyTestDbContext _context) : ControllerBase
   [HttpPost("[action]/{id}")]
   public async Task<IActionResult> Update(int id, Customer customer)
   {
-    if (id != customer.Id)
-    {
-      return BadRequest();
-    }
-
-    _context.Entry(customer).State = EntityState.Modified;
-
     try
     {
-      await _context.SaveChangesAsync();
-    }
-    catch (DbUpdateConcurrencyException)
-    {
-      if (!CustomerExists(id))
-      {
-        return NotFound();
-      }
-      else
-      {
-        throw;
-      }
-    }
+      if (id != customer.Id)
+        return BadRequest();
 
-    return NoContent();
+      var info = await _context.Customers.FindAsync(customer.Id);
+      if (info == null)
+        return NotFound();
+
+      // update
+      info.Name = customer.Name;
+      info.Email = customer.Email;
+      // auto update system filed
+      info.UpdatedAt = DateTimeOffset.Now;
+
+      //_context.Entry(customer).State = EntityState.Modified;
+      await _context.SaveChangesAsync();
+      return NoContent();
+    }
+    catch(Exception ex)
+    {
+      return UnprocessableEntity(new
+      {
+        ex.Message,
+        ex.StackTrace
+      });
+    }
   }
 
   [HttpPost("[action]")]
   public async Task<ActionResult<Customer>> Create(Customer customer)
   {
+    var now = DateTimeOffset.Now;
+    customer.CreatedAt = now;
+    customer.UpdatedAt = now;
+
     _context.Customers.Add(customer);
     await _context.SaveChangesAsync();
-
-    return CreatedAtAction("GetCustomer", new { id = customer.Id }, customer);
+    return CreatedAtAction("Read", new { id = customer.Id }, customer);
   }
 
   [HttpPost("[action]/{id}")]
