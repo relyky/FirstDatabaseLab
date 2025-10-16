083946fd2ad8131ac3510b2a623a1b58dfcbbd28
Rely Ky <relyky@hotmail.com>
2025-10-16 10:28:20 +0800

    20251016020516_AddDeveloper

--- FirstDatabaseLab.DB/Migrations/20251016020516_AddDeveloper.Designer.cs
+++ FirstDatabaseLab.DB/Migrations/20251016020516_AddDeveloper.Designer.cs
@@ -4,6 +4,7 @@ using FirstDatabaseLab.DB.Schema;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FirstDatabaseLab.DB.Migrations
 {
     [DbContext(typeof(MyTestDbContext))]
-    partial class MyTestDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251016020516_AddDeveloper")]
+    partial class AddDeveloper
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- FirstDatabaseLab.DB/Schema/Product.cs
+++ FirstDatabaseLab.DB/Schema/Product.cs
@@ -8,15 +8,18 @@ namespace FirstDatabaseLab.DB.Schema;
 
 public partial class Product
 {
-    [Key]
-    public int ProductNo { get; set; }
+  [Key]
+  public int ProductNo { get; set; }
 
-    [StringLength(50)]
-    public string Name { get; set; } = null!;
+  [StringLength(50)]
+  public string Name { get; set; } = null!;
 
-    [Column(TypeName = "decimal(18, 4)")]
-    public decimal Price { get; set; }
+  [Column(TypeName = "decimal(18, 4)")]
+  public decimal Price { get; set; }
 
-    [InverseProperty("ProductNoNavigation")]
-    public virtual ICollection<OrderItem> OrderItem { get; set; } = new List<OrderItem>();
+  [StringLength(50)]
+  public string Developer { get; set; } = null!;
+
+  [InverseProperty("ProductNoNavigation")]
+  public virtual ICollection<OrderItem> OrderItem { get; set; } = new List<OrderItem>();
 }
